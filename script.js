84485be31d3fa8cc34b5c230ff573f7bd81fa243
Sandeep Subedi <sandeep1111able@gmail.com>
2021-11-15 20:20:03 +0545

    Added images

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
-function computerPlay(){
-    const computerHand = ["Rock","Paper","Scissors"];
-    const random = Math.floor(Math.random()*computerHand.length);
+const buttons = document.querySelectorAll(".btn-rps");
+let player = document.querySelector("#player-score");
+let computer = document.querySelector("#computer-score");
+const playerChoice = document.querySelector(".player-choice");
+const computerChoice = document.querySelector(".computer-choice");
+let playerScore = 0,
+    computerScore = 0;
+
+
+
+function computerPlay() {
+    const computerHand = ["Rock", "Paper", "Scissors"];
+    const random = Math.floor(Math.random() * computerHand.length);
     return computerHand[random].toLowerCase();
 }
 
-function playRound(playerSelection,computerSelection){
-    if(playerSelection==="rock" && computerSelection==="paper" ){
-        return "You Lose! Paper beats Rock";
-    }
-    else if (playerSelection==="paper" && computerSelection==="scissors"){
-        return "You Lose! Scissors beats paper";
-    }
-    else if (playerSelection==="scissors" && computerSelection==="rock"){
-        return "You Lose! Scissors beats paper";
-    }
-    else if (playerSelection===computerSelection){
-        return `It's a draw! You both picked ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`;
-    }
-    else{
-        return `You Won! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`;
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return computer.textContent = ++computerScore;
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return computer.textContent = ++computerScore;
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return computer.textContent = ++computerScore;
+    } else if (playerSelection === computerSelection) {
+
+    } else {
+        return player.textContent = ++playerScore;
     }
 
 }
 
-function game(){
-    for(let i =0; i<5;i++){
-    const userInput = prompt('Please Enter Rock,Paper or Scissors').toLowerCase();
-    console.log(playRound(userInput,computerPlay()));
-    }
-}
-
-game();
-
+buttons.forEach((button) => {
+    button.addEventListener('click', function(e) {
+        let playerValue = e.target.textContent.toLowerCase();
+        let computerValue = computerPlay();
+        playRound(playerValue, computerValue);
+        playerChoice.style.backgroundImage = `url('./images/${playerValue}.png')`;
+        computerChoice.style.backgroundImage = `url('./images/${computerValue}.png')`;
+        if (playerScore === 5) {
+            alert("You Win");
+            location.reload();
+        } else if (computerScore === 5) {
+            alert("You Lose");
+            location.reload();
+        }
+    });
+});
